3097798cd5c446108cd697f23761bdaeb116f38c
Ivan Kruglov <mail@ikruglov.com>
2022-11-17 23:01:33 +0100

    compile-shelly1.sh

--- compile-shelly1.sh
+++ compile-shelly1.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-cat user_config_override_shelly1.h my_user_config_settings.h > user_config_override.h
-./compile.sh $*
+if [ -f my_user_config_settings.h ]; then
+    cat user_config_override_shelly1.h my_user_config_settings.h > user_config_override.h
+    sh -x ./compile.sh $*
+    exit $?
+else
+    echo "no my_user_config_settings.h"
+    exit 1
+fi
