5d6e773eaca4e1c1c700cff775d2bc2f73656be8
Ivan Kruglov <mail@ikruglov.com>
2022-11-18 17:45:09 +0100

    compile-shelly1.sh

--- compile-shelly1.sh
+++ compile-shelly1.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+
+cp local_ca_d* Tasmota/tasmota/include/
+
 if [ -f my_user_config_settings.h ]; then
     cat user_config_override_shelly1.h my_user_config_settings.h > user_config_override.h
-    sh -x ./compile.sh $*
+    sh ./compile.sh $*
     exit $?
 else
     echo "no my_user_config_settings.h"
